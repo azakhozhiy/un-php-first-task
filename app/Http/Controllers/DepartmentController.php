38032b9f1b9698c8fad42eb28071643f6ad1a7f4
Dikshant <dikshantraj2001@gmail.com>
2023-03-16 18:44:48 +0530

    All tasks done

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -7,23 +7,32 @@ namespace App\Http\Controllers;
 use App\Contract\EmployeeRepositoryInterface;
 use App\Enums\EmployeeStatusEnum;
 use App\Models\Department;
+use App\Models\Employee;
 use Illuminate\Http\JsonResponse;
 
 class DepartmentController extends Controller
 {
+    private $repo;
+
+    public function __construct(EmployeeRepositoryInterface $employeeRepository)
+    {
+        $this->repo = $employeeRepository;
+    }
+
     /**
      * TODO: 1. First issue. You need to bind a concrete instance of EmployeeRepositoryInterface
      */
-    public function getActiveEmployees(EmployeeRepositoryInterface $employeeRepository, int $departmentId): JsonResponse
+    public function getActiveEmployees(Department $department): JsonResponse
     {
         /**
          * TODO: 2. Second issue. Need find concrete department by $departmentId
          *
          * @var Department $department
          */
-        $department = Department::query()->first();
 
-        $employees = $employeeRepository->findActiveByDepartment($department);
+        //  For achieveing this, I need to change the route parameter and rename it to department like /{department}, It won't work with /{id} -> Route Model Binding
+
+        $employees = $this->repo->findActiveByDepartment($department);
 
         return response()->json($employees);
     }
@@ -35,14 +44,27 @@ class DepartmentController extends Controller
          *
          * @var Department $department
          */
-        $department = Department::query()->first();
+
+        // Not using route model binding here but it is good to use it
+        $department = Department::FindOrFail($departmentId);
+
+        // We can even use EmployeeRepositoryInterface for this, I had created an extra method there
+
+        // $this->repo->blockEmployeesByDepartment($department);
 
         /**
          * TODO: 5. Five issue. We need to block all employees of the department
          * @see EmployeeStatusEnum
          */
 
+        // one option is that we can achieve this by using relationship updation
 
-        return response()->json();
+        // $department->employees()->update(['status' => EmployeeStatusEnum::BLOCKED->id()]);
+
+        // another option is to just update employee model from here
+        Employee::where('department_id', $department->id)
+                ->update(['status' => EmployeeStatusEnum::BLOCKED->id()]);
+        
+        return response()->json(['success' => true]);
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Contract\EmployeeRepositoryInterface;
+use App\Repository\EmployeeRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app->bind(EmployeeRepositoryInterface::class, EmployeeRepository::class);
     }
 }
